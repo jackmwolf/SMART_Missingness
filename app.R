bbd4f297f0fda0fd8cdea961cadfc17309f6c554
Jack M Wolf <38739875+jackmwolf@users.noreply.github.com>
2021-04-20 11:36:17 -0500

    Create app skeleton with input and output

---
+++ app.R
@@ -0,0 +1,76 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(dplyr)
+load("SMARTdat.rda")
+source("funcs.R")
+true.dat <- setup(t)
+
+
+metric_list <- c("MSE", "Coverage Probability")
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("SMART Missingness Simulator"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("pct_missing",
+                        "% Missing Data:",
+                        min = 0,
+                        max = 99,
+                        value = 10),
+            radioButtons("mechanism",
+                         "Missingness Mechanism",
+                         c("MCAR", "MAR", "MNAR"),
+                         selected = "MAR"),
+            numericInput("n",
+                        "Sample Size:",
+                        min = 100,
+                        max = 1000,
+                        value = 200),
+            numericInput("n_sims",
+                         "Number of Simulations:",
+                         value = 50,
+                         min = 2,
+                         max = 100),
+            checkboxGroupInput("metrics",
+                               "Which metrics?",
+                               metric_list,
+                               selected = metric_list)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           textOutput("test_message")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$test_message <- renderText(
+        paste0("You have selected ",
+               input$pct_missing, "% missingness, ",
+               input$mechanism, " as the missingness mechanism, and ",
+               input$n, " for the sample size. ",
+               "To be carried out over ",
+               input$n_sims, " simulations with the following metrics displayed: ",
+               paste0(input$metrics, collapse = ", "), ".")
+    )
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ funcs.R
@@ -0,0 +1,52 @@
+
+main <- function(n, pct_missing, mechanism, n_sims) {
+  
+  
+  # Aggregate over n_sims repititions
+  {
+    # Take a sample of size n from the population
+    sim.dat <- sample_n(true.dat, n, replace = FALSE)
+    
+    # Induce missingness
+    
+    # Perform MI
+    
+  }
+  
+}
+
+
+
+# Function to set up data
+setup <- function(mi.dat) {
+  # Delete P treatment
+  d <- mi.dat
+  mi.dat <- d[-which(d$TREAT.2 == "Perphenazine"), ]
+  mi.dat <- mi.dat[-which(mi.dat$TREAT.1 == "Perphenazine"), ]
+  n <- length(mi.dat$CATIEID)
+  # Define dummy variables
+  mi.dat$va <- ifelse(mi.dat$SITETYPE == "VA", 1, 0)
+  mi.dat$combo <- ifelse(mi.dat$SITETYPE == "Combo", 1, 0)
+  mi.dat$uni <- ifelse(mi.dat$SITETYPE == "University Clinic", 1, 0)
+  mi.dat$state <- ifelse(mi.dat$SITETYPE == "State Mental Health", 1, 0)
+  mi.dat$EMPLOY <- ifelse(mi.dat$EMPLOY == "Did Not Work", 0, 1)
+  mi.dat$EXACER <- ifelse(mi.dat$EXACER == "Yes", 1, 0)
+  mi.dat$SEX <- ifelse(mi.dat$SEX == "Male", 1, 0)
+  mi.dat$TD <- ifelse(mi.dat$TD == "Yes", 1, 0)
+  # Treatment 1 dummies
+  mi.dat$qtrt1 <- ifelse(mi.dat$TREAT.1 == "Quetiapine", 1, 0)
+  mi.dat$otrt1 <- ifelse(mi.dat$TREAT.1 == "Olanzapine", 1, 0)
+  mi.dat$rtrt1 <- ifelse(mi.dat$TREAT.1 == "Risperidone", 1, 0)
+  mi.dat$ptrt1 <- ifelse(mi.dat$TREAT.1 == "Perphenazine", 1, 0)
+  # Treatment 2 dummies
+  mi.dat$qtrt2 <- ifelse(mi.dat$TREAT.2 == "Quetiapine", 1, 0)
+  mi.dat$otrt2 <- ifelse(mi.dat$TREAT.2 == "Olanzapine", 1, 0)
+  mi.dat$ctrt2 <- ifelse(mi.dat$TREAT.2 == "Clozapine", 1, 0)
+  mi.dat$rtrt2 <- ifelse(mi.dat$TREAT.1 == "Risperidone", 1, 0)
+  mi.dat$ztrt2 <- ifelse(mi.dat$TREAT.2 == "Ziprasidone", 1, 0)
+  mi.dat$SWITCH <- ifelse(mi.dat$SWITCH == "SWITCHED", 1, 0)
+  mi.dat$STAGE2_ARM <- ifelse(mi.dat$STAGE2_ARM == "Efficacy", 1, 0) # 1 is efficacy
+  mi.dat$pan <- (mi.dat$PANSSTOT.0 + mi.dat$PANSSTOT.1 + mi.dat$PANSSTOT.2) / 3
+  levels(mi.dat$TREAT.2)[levels(mi.dat$TREAT.2) == "Ziprasidone" | levels(mi.dat$TREAT.2) == "Clozapine"] <- "Clz"
+  return(mi.dat)
+}
